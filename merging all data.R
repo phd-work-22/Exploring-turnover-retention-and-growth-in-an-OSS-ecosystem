304532e1e65c4d41813d7d29380812b7e97ee1e9
our-work-list <132989134+phd-work-22@users.noreply.github.com>
2025-03-24 08:41:55 +0100

    Update merging all data.R

--- merging all data.R
+++ merging all data.R
@@ -1,14 +1,13 @@
 library(dplyr)
 library(ggplot2)
 
-#setwd("/Users/tientulili/Documents/My Doctorate FILES/2 PhD Objective 2/Replicate Study/threads")
-#mailists <- read.csv("2022-05-1m.csv",TRUE, ",")
 
-setwd("/Users/tientulili/Documents/gentoo_mailingLists/completed mailing lists reports/")
+
+setwd("path/to_file/")
 mailists <- read.csv("dumpFile.csv",TRUE, ",")
 
 df <- as.data.frame(mailists)
-for(year in 2001:2022) {
+for(year in 2001:2023) {
   for(month in 1:12) {
     for(page in 1:10) {
       if (month < 10) {
@@ -41,7 +40,7 @@ for(year in 2001:2022) {
   } 
 }
 
-write.csv(df,"/Users/tientulili/Documents/gentoo_mlists_all.csv", row.names = FALSE)
+write.csv(df,"/path/gentoo_mlists_all.csv", row.names = FALSE)
 df
 
 
